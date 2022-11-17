d2ac34b68d2200be3a3ecdeed2655cd9f632de4f
agarwalchirag19 <botxchirag@gmail.com>
2022-11-17 22:32:43 +0530

    upadte

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,13 +1,12 @@
 import Navbar from "../components/Navbar";
 import React from "react";
-import { FaEthereum } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500">
-        <div className="text-2xl text-red-700 px-[100px] py-[60px] ">
+      <div className="cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 py-[628.5px]">
+        <div className="text-2xl text-red-700 ">
           <div></div>
         </div>
       </div>
