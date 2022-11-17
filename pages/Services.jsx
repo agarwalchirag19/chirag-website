8e4528646c3bb51173fb5b4ec3f5e78b33cd27d8
agarwalchirag19 <botxchirag@gmail.com>
2022-11-17 21:49:15 +0530

    update

--- pages/Services.jsx
+++ pages/Services.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
             <div class="container px-5 py-24 mx-auto">
               <div class="flex flex-col text-center w-full mb-20">
                 <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">
-                  Pricing
+                  Pricing 😀
                 </h1>
                 <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
                   The best pricing you can find is only at Crypto Cloud
@@ -25,7 +25,7 @@ const Contact = () => {
               </div>
               <div class="flex flex-wrap -m-4">
                 <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                  <div class="h-full p-6 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden">
+                  <div class="h-full p-6 rounded-lg border-2 border-blue-700 flex flex-col relative overflow-hidden">
                     <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">
                       BASIC
                     </h2>
@@ -85,7 +85,7 @@ const Contact = () => {
                       </span>
                       Octopus Algo : 20 M/Hs
                     </p>
-                    <button class="flex items-center mt-auto text-white bg-gray-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-700 rounded">
+                    <button class="flex items-center mt-auto text-white bg-blue-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-blue-700 rounded">
                       0.013
                       <svg
                         fill="none"
@@ -103,8 +103,8 @@ const Contact = () => {
                   </div>
                 </div>
                 <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                  <div class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
-                    <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
+                  <div class="h-full p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
+                    <span class="bg-red-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
                       POPULAR
                     </span>
                     <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">
@@ -182,7 +182,7 @@ const Contact = () => {
                       </span>
                       Octopus Algo : 40 M/Hs
                     </p>
-                    <button class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
+                    <button class="flex items-center mt-auto text-white bg-red-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-600 rounded">
                       0.035
                       <svg
                         fill="none"
@@ -202,7 +202,7 @@ const Contact = () => {
                   </div>
                 </div>
                 <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                  <div class="h-full p-6 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden">
+                  <div class="h-full p-6 rounded-lg border-2 border-blue-700 flex flex-col relative overflow-hidden">
                     <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">
                       BUSINESS
                     </h2>
@@ -294,7 +294,7 @@ const Contact = () => {
                       </span>
                       Octopus Algo : 60 M/Hs
                     </p>
-                    <button class="flex items-center mt-auto text-white bg-gray-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-700 rounded">
+                    <button class="flex items-center mt-auto text-white bg-blue-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-blue-700 rounded">
                       0.068
                       <svg
                         fill="none"
@@ -314,7 +314,7 @@ const Contact = () => {
                   </div>
                 </div>
                 <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                  <div class="h-full p-6 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden">
+                  <div class="h-full p-6 rounded-lg border-2 border-blue-700 flex flex-col relative overflow-hidden">
                     <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">
                       SPECIAL
                     </h2>
@@ -405,7 +405,7 @@ const Contact = () => {
                       </span>
                       Octopus Algo : 100 M/Hs
                     </p>
-                    <button class="flex items-center mt-auto text-white bg-gray-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-700 rounded">
+                    <button class="flex items-center mt-auto text-white bg-blue-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-blue-700 rounded">
                       0.101
                       <svg
                         fill="none"
