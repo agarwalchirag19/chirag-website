6a716c91fda79388a4bd87500d1c40726f52ec48
agarwalchirag19 <botxchirag@gmail.com>
2022-11-18 13:34:29 +0530

    update

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -6,9 +6,7 @@ const Contact = () => {
     <div>
       <Navbar />
       <div className="cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 py-[628.5px]">
-        <div className="text-2xl text-red-700 ">
-          <div></div>
-        </div>
+        <div className="text-2xl text-red-700 "></div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Login from './Login';
+import Login from "./Login";
 import Navbar from "../components/Navbar";
 import { useMetaMask } from "metamask-react";
 
@@ -42,10 +42,46 @@ export default function Home() {
           Connected account {account} on chain ID {chainId}
         </button>
         <Navbar />
-        <div className="cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 ">
-          <h1 className="text-2xl text-red-700 px-[200px] py-[612.5px] ">
-            fuyasygfifb
-          </h1>
+        <div className="cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 px-[100px] py-[150px]  ">
+          <div className="flex flex-row">
+            <div className="flex flex-row w-[70rem] border-[6px] rounded overflow-hidden transfrom duration-500 hover:scale-90 ">
+              <h1 className="text-[30px] flex text-center align-text text-black indent-1">
+                Bitcoin mining refers to ensuring that transactions are valid
+                and added to the Bitcoin blockchain correctly using a global
+                network of computers running the Bitcoin code. The process of
+                mining is also the means by which new Bitcoins are created. The
+                process of bitcoin mining involves the verification of new
+                transactions against the Bitcoin network, which results in the
+                production of new bitcoins.Bitcoin mining is the process by
+                which Bitcoin transactions are validated digitally on the
+                Bitcoin network and added to the blockchain ledger.t is done by
+                solving complex cryptographic hash puzzles to verify blocks of
+                transactions that are updated on the decentralized blockchain
+                ledger.Solving these puzzles requires powerful computing power
+                and sophisticated equipment. In return, miners are rewarded with
+                Bitcoin, which is then released into circulation hence the name
+                Bitcoin mining.Cryptocurrency mining is a process of creating
+                new digital "coins." However, that is as far as simplicity goes.
+                The process of recovering these coins requires solving complex
+                puzzles, validating cryptocurrency transactions on a blockchain
+                network and adding them to a distributed ledger to locate them.
+              </h1>
+            </div>
+            <h1 className="text-[2rem] align-text text-center w-[40rem] text-white">
+              PROS
+              <br />
+              <br />
+              <br />
+             1 . No restrictions on payment
+            <br />
+            2 . Maintainance of Anoniminty
+              <br />
+              
+            </h1>
+            <h1 className="text-[2rem] align-text text-center w-[40rem] text-white">
+              CONS
+            </h1>
+          </div>
         </div>
       </div>
     );
