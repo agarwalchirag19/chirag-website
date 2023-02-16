d2c20e308ae6dac2e14dd0b1e2032efff7942e47
agarwalchirag19 <botxchirag@gmail.com>
2023-02-16 19:24:27 +0530

    update

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,24 +1,12 @@
-import {
-  FaFacebook,
-  FaFacebookF,
-  FaGoogle,
-  FaLinkedinIn,
-  FaRegEnvelope,
-} from "react-icons/fa";
-
 import Footer from "../components/Footer";
 import Head from "next/head";
-import Image from "next/image";
-import { MdLockOutline } from "react-icons/md";
 import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
 
 const Contact = () => {
-  return (
+  return(
     <div>
       <Navbar/>
       <div className='cursor-default bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500'>
-      
       </div>
       <Footer/>
     </div>
