a94c59f4effae7318be57e33f0bb1d4a726c82a5
agarwalchirag19 <botxchirag@gmail.com>
2023-02-17 07:41:44 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from "../components/footer";
 import Login from "./Login";
 import Navbar from "../components/Navbar";
 import { useMetaMask } from "metamask-react";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   const { status, connect, account, chainId, ethereum } = useMetaMask();
@@ -55,25 +56,15 @@ export default function Home() {
           <div className="flex flex-row">
             <div className="flex flex-row w-[70rem] overflow-hidden transfrom duration-500 hover:scale-90 p-10 bg-black">
               <h1 className="text-[30px] flex text-center align-text text-white indent-1">
-                Bitcoin mining refers to ensuring that transactions are valid
-                and added to the Bitcoin blockchain correctly using a global
-                network of computers running the Bitcoin code. The process of
-                mining is also the means by which new Bitcoins are created. The
-                process of bitcoin mining involves the verification of new
-                transactions against the Bitcoin network, which results in the
-                production of new bitcoins.Bitcoin mining is the process by
-                which Bitcoin transactions are validated digitally on the
-                Bitcoin network and added to the blockchain ledger.t is done by
-                solving complex cryptographic hash puzzles to verify blocks of
-                transactions that are updated on the decentralized blockchain
-                ledger.Solving these puzzles requires powerful computing power
-                and sophisticated equipment. In return, miners are rewarded with
-                Bitcoin, which is then released into circulation hence the name
-                Bitcoin mining.Cryptocurrency mining is a process of creating
-                new digital "coins." However, that is as far as simplicity goes.
-                The process of recovering these coins requires solving complex
-                puzzles, validating cryptocurrency transactions on a blockchain
-                network and adding them to a distributed ledger to locate them.
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Bitcoin mining refers to ensuring that transactions are valid and added to the Bitcoin blockchain correctly using a global network of computers running the Bitcoin code. The process of mining is also the means by which new Bitcoins are created. The process of bitcoin mining involves the verification of new transactions against the Bitcoin network, which results in the production of new bitcoins.Bitcoin mining is the process by which Bitcoin transactions are validated digitally on the Bitcoin network and added to the blockchain ledger.it is done by solving complex cryptographic hash puzzles to verify blocks of transactions that are updated on the decentralized blockchain ledger.Solving these puzzles requires powerful computing power and sophisticated equipment. In return, miners are rewarded with Bitcoin, which is then released into circulation hence the Bitcoin mining . Cryptocurrency mining is a process of creating new digital coins. However, that is as far as simplicity goes. The process of recovering these coins requires solving complex puzzles, validating cryptocurrency transactions on a blockchain network and adding them to a distributed ledger to locate them.",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
               </h1>
             </div>
             <h1 className="text-[2rem] align-text text-center w-[40rem] text-white px-10">
@@ -146,8 +137,8 @@ export default function Home() {
             </h1>
           </div>
         </div>
-        <div className='text-black'>
-        <Footer/>
+        <div className="text-black">
+          <Footer />
         </div>
       </div>
     );
